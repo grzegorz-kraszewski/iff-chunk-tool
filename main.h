6659986deb5ca978ec24192a5563927fbe8fd21f
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2025-05-27 22:58:03 +0200

    Small improvements of the skeleton.

--- main.h
+++ main.h
@@ -2,7 +2,9 @@
 
 #include <proto/locale.h>
 
+//===============
 // integer types
+//===============
 
 typedef signed char int8;
 typedef unsigned char uint8;
@@ -11,20 +13,29 @@ typedef unsigned short uint16;
 typedef signed long int32;
 typedef unsigned long uint32;
 
+//======================
 // global library bases
+//======================
 
 extern Library
 	*SysBase,
 	*DOSBase,
 	*IFFParseBase;
 
+//=================
+// error reporting
+//=================
+
+int32 Problem(const char *msg);
+
+//===============
 // locale stuff
+//===============
 
 extern Library *LocaleBase;
 extern Catalog *Cat;
-extern Locale *Loc;
 
-static inline char* LS(LONG id, char* defstr)
+static inline char* LS(int32 id, char* defstr)
 {
 	return (Cat ? GetCatalogStr(Cat, id, defstr) : defstr);
 }
