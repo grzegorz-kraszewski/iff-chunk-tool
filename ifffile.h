c6f079940224e44a46800663c3717ffa7b3174c1
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2025-05-28 20:59:08 +0200

    Added operator IFFHandle*, so IFFFile instances can be used in iffparse.library calls.

--- ifffile.h
+++ ifffile.h
@@ -8,24 +8,30 @@
 #include "main.h"
 
 #include <dos/dos.h>
-#include <libraries/iffparse.h>
+#include <proto/iffparse.h>
+
+//=============================================================================
+// Instances of IFFFile (and subclasses) may be used as IFFHandle in calls to 
+// iffparse.library functions, thanks to operator IFFHandle*().
+//=============================================================================
 
 class IFFFile
 {
 	protected:
 
 	BPTR handle;
-	IFFHandle *iff;
 	const char* path;
 	bool opened;
+	IFFHandle *iff;
 	bool FileProblem() { return SysProblem(path); }
-	bool IFFProblem() { return Problem("arghh"); }
 
 	public:
 
+	operator IFFHandle*() { return iff; }
 	IFFFile() : handle(NULL), path(NULL), iff(NULL), opened(FALSE) {}
 	bool OpenFile(const char *filepath, int32 mode);
 	const char* FileName() { return path; }
+	bool IFFProblem() { return Problem("arghh"); }
 	~IFFFile();
 };
 
