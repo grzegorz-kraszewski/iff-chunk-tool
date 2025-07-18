fa53aef3da5d880b16108601a9b3d52ed12a4007
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2025-07-18 22:21:49 +0200

    Revision bumped to 1.0

--- start.cpp
+++ start.cpp
@@ -69,7 +69,7 @@ __saveds ULONG Start(void)
 }
 
 
-__attribute__((section(".text"))) UBYTE VString[] = "$VER: IFFChunkTool 0.2 (15.07.2025)\r\n";
+__attribute__((section(".text"))) UBYTE VString[] = "$VER: IFFChunkTool 1.0 (18.07.2025)\r\n";
 
 
 
